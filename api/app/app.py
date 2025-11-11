b46f21a96725cfff316cbc222114fa1fc7525864
Yılmaz Aygın <yilmazaygin@gmail.com>
2025-11-11 21:19:05 +0300

    I accidently wrote qrmenu...

--- api/app/app.py
+++ api/app/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from api.app.routers import test_router, calculator_router
 
-app = FastAPI(title="Advanced QR Menu API", version="0.1")
+app = FastAPI(title="Solar Project", version="0.1")
 
 # Include routers
 app.include_router(test_router.router)
@@ -10,4 +10,4 @@ app.include_router(calculator_router.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to Advanced QR Menu API"}
+    return {"message": "Welcome to Solar Project API"}
